f405f9ba198821f7f19ff5fedd88de05f69b4da1
Brian Kulyk <brian.kulyk@lgm.ca>
2019-07-26 16:59:23 -0700

    try expansion panel base off of -- https://codesandbox.io/s/ufy23

---
+++ src/components/expansion-panel/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './expansion-panel.css';
+
+const ExpansionPanel = () => {
+  <div className="panel">
+    <div>Summary</div>
+    <div>
+      <p>Details</p>
+      <p>Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas eros, vitae egestas augue. Duis vel est augue.</p>
+    </div>
+  </div>
+};
+
+export default ExpansionPanel;
