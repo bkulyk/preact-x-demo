070453c755be682da29fbab534467fc33e01b08f
Brian Kulyk <brian@kulyk.ca>
2019-07-28 11:09:31 -0700

    pull out app bar links, move all app bar implementation from app to app-bar-demo, add fix for the drawer exploding when there is no active links

--- src/components/app.js
+++ src/components/app.js
@@ -1,31 +1,25 @@
-import React, { useState } from 'react';
-import AppBar from './app-bar';
+import React from 'react';
+import AppBarDemo from './demo/app-bar-demo';
 import CardDemo from './demo/card-demo';
-import Drawer from './drawer';
 import ButtonDemo from './demo/button-demo';
 import ExpansionDemo from './demo/expansion-demo';
 
-const App = () => {
-  const [open, setOpen] = useState(false);
+const App = () => (
+  <div>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+    <link href="/material-components-web.min.css" rel="stylesheet" />
+    <link href="/whatever.css" rel="stylesheet" />
 
-  return (
-    <div>
-      <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet" />
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
-      <link href="/material-components-web.min.css" rel="stylesheet" />
-      <link href="/whatever.css" rel="stylesheet" />
+    <AppBarDemo />
 
-      <AppBar onNav={() => setOpen(!open)} title="Preacting" />
-      <Drawer open={open} onClose={() => setOpen(false)} />
-
-      <main style={{ backgroundColor: '#f5f5f5', padding: '16px' }}>
-        <h1>Demo Page</h1>
-        <ExpansionDemo />
-        <CardDemo />
-        <ButtonDemo />
-      </main>
-    </div>
-  );
-};
+    <main style={{ backgroundColor: '#f5f5f5', padding: '16px' }}>
+      <h1>Demo Page</h1>
+      <ExpansionDemo />
+      <CardDemo />
+      <ButtonDemo />
+    </main>
+  </div>
+);
 
 export default App;
--- src/components/drawer.js
+++ src/components/drawer.js
@@ -1,11 +1,24 @@
 import React, { useRef, useEffect, Fragment } from 'react';
+import PropTypes from 'prop-types';
 import { MDCDrawer } from "@material/drawer";
 
-const Drawer = ({ open, onClose, onOpen }) => {
+const Drawer = ({
+  open,
+  onClose,
+  onOpen,
+  children,
+}) => {
   let drawer;
   const drawerRef = useRef();
 
   useEffect(() => {
+    const activated = drawerRef.current.getElementsByClassName('mdc-list-item--activated').length;
+    if (!activated) {
+      const allItems = drawerRef.current.getElementsByClassName('mdc-list-item');
+      // if nothing is activated, the the drawer will throw an error
+      allItems[0].className += "  mdc-list-item--activated";
+    }
+
     drawer = MDCDrawer.attachTo(drawerRef.current);
     drawer.open = open;
     drawer.listen('MDCDrawer:closed', onClose);
@@ -16,26 +29,28 @@ const Drawer = ({ open, onClose, onOpen }) => {
     <Fragment>
       <aside className="mdc-drawer mdc-drawer--modal" ref={drawerRef}>
         <div className="mdc-drawer__content">
-          <nav className="mdc-list">
-            <a className="mdc-list-item mdc-list-item--activated" href="#" aria-current="page">
-              <i className="material-icons mdc-list-item__graphic" aria-hidden="true">inbox</i>
-              <span className="mdc-list-item__text">Inbox</span>
-            </a>
-            <a className="mdc-list-item" href="#">
-              <i className="material-icons mdc-list-item__graphic" aria-hidden="true">send</i>
-              <span className="mdc-list-item__text">Outgoing</span>
-            </a>
-            <a className="mdc-list-item" href="#">
-              <i className="material-icons mdc-list-item__graphic" aria-hidden="true">drafts</i>
-              <span className="mdc-list-item__text">Drafts</span>
-            </a>
-          </nav>
+          {children}
         </div>
       </aside>
-
       <div className="mdc-drawer-scrim"></div>
     </Fragment>
   );
 };
+
+Drawer.propTypes = {
+  open: PropTypes.bool,
+  onClose: PropTypes.func,
+  onOpen: PropTypes.func,
+  header: PropTypes.node,
+  children: PropTypes.node,
+}
+
+Drawer.defaultProps = {
+  open: false,
+  onClose: () => {},
+  onOpen: () => {},
+  header: '',
+  children: ''
+}
 
 export default Drawer;
