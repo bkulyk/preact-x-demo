a44b328cf41faaca9078c4192f9030c73a0887d4
Brian Kulyk <brian@kulyk.ca>
2019-08-11 20:43:07 -0700

    test for the transition component

---
+++ __tests__/transition.spec.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Transition from '@bitchin/transition';
+import { shallow } from './enzyme';
+import { it, expect } from './jest';
+
+it('should render correctly', () => {
+  const wrapper = shallow((
+    <Transition>
+      {(state) => (
+        <div>{state}</div>
+      )}
+    </Transition>
+  ));
+  expect(wrapper.text()).toEqual('exited');
+
+  wrapper.setProps({ on: true });
+  expect(wrapper.text()).toEqual('entered');
+});
